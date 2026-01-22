f97ac7e2150c009e4367ec8a1479f32d4ec45429
Alex de Luna <67716472+alexdeluna@users.noreply.github.com>
2026-01-22 16:04:00 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,30 @@
 const CACHE_NAME = 'controle-diario-v3';
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); // força ativação
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
-        './',
         './index.html',
-        './app.js',
-        './manifest.json'
+        './app.js'
       ]);
     })
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim(); // assume controle imediato
 });
 
 self.addEventListener('fetch', event => {
